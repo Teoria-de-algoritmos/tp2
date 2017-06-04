9429d9a886915d5e9a1a3b6bd1e1df57700844ab
Alejo Cova <covaalejo@gmail.com>
2017-06-04 17:46:17 -0300

    Main
    Se comenzo el archivo main y se quitaron los ejemplos de geeksforgeeks

---
+++ Examples/main.py
@@ -0,0 +1,33 @@
+# Ejemplo
+from timeit import default_timer as timer
+import BellmanFord, FloydWarshall, Dijkstra, GrafoUtils
+
+
+quant = input("Ingrese la cantidad de monedas:")
+G = generarGrafo(quant)
+
+print("Vertices: "+str(k1.grafo.vertices()))
+
+start = timer()
+D = Dijkstra(G)
+end = timer()
+
+
+print("Dijkstra: "+str((end - start)*1000)+" mseg")
+
+start = timer()
+B = BellamFord(G)
+end = timer()
+
+print("BellmanFord: "+str((end - start)*1000)+" mseg")
+
+start = timer()
+F = FloydWarshall(G)
+end = timer()
+
+print("FlkoydWarshall: "+str((end - start)*1000)+" mseg")
+
+print D.distancia()
+print B.distancia()
+print F.distancia()
+
