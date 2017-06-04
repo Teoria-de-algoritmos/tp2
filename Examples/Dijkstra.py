b550e4c237c124c777841b5c51d4fcf7cf01d286
NKeklikian <nkeklikian@gmail.com>
2017-06-04 19:54:06 -0300

    Más errores chicos

--- Examples/Dijkstra.py
+++ Examples/Dijkstra.py
@@ -8,15 +8,13 @@ class Dijkstra:
 
 		for i in xrange(grafo.vertices()):
 			self.distancias[i][i] = 0
-			for j in xrange(grafo.vertices()):
-				cola.append((self.distancias[i][j], j))
-			cola.sort()
+			cola = sorted([(self.distancias[i][j], j) for j in xrange(grafo.vertices())])
 			while cola:
 				actual = cola.pop(0)
 				for siguiente in xrange(self.grafo.vertices()):
 					nuevaDistancia = self.distancias[i][actual] + self.distancias[actual][siguiente]
 					if nuevaDistancia < self.distancias[i][siguiente]:
-						cola.remove((distancia[i][siguiente], siguiente))
+						cola.remove((self.distancias[i][siguiente], siguiente))
 						self.distancias[i][siguiente] = nuevaDistancia
 						insort(cola, (nuevaDistancia, siguiente))
 	
