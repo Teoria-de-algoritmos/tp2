d63e56088dbbc10b3579709299d20f171b466202
NKeklikian <nkeklikian@gmail.com>
2017-06-04 19:56:00 -0300

    Indent

--- Examples/FloydWarshall.py
+++ Examples/FloydWarshall.py
@@ -1,13 +1,12 @@
 from GrafoUtils import Digrafo
 
-
 class FloydWarshall:
 
     def __init__(self, grafo):
         self.grafo = grafo
         self.dist = [[grafo.peso(i,j) for j in xrange(grafo.vertices())] for i in xrange(grafo.vertices())]
         for k in xrange(grafo.vertices()):
-             for i in xrange(grafo.vertices()):
+            for i in xrange(grafo.vertices()):
                 for j in xrange(grafo.vertices()):
                     self.dist[i][j] = min(self.dist[i][j], self.dist[i][k]+ self.dist[k][j])
 
