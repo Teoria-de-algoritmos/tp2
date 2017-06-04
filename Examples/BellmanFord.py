225fa6edc49381bfe42d89930e2b5a67662d3b45
NKeklikian <nkeklikian@gmail.com>
2017-06-04 20:00:53 -0300

    Corrijo indentación

--- Examples/BellmanFord.py
+++ Examples/BellmanFord.py
@@ -1,26 +1,24 @@
-
-
 class BellmanFord: 
 
     def __init__(self,grafo):
         self.grafo = grafo
         self.distancias = []
-	    for src in xrange(self.grafo.vertices()):	 
-	        dist = [float("Inf")] * self.grafo.vertices()
-	        dist[src] = 0
-	        for _ in xrange(self.grafo.vertices() - 1):
-	            for u in xrange(self.grafo.vertices()):
-	            	for v in xrange(self.grafo.vertices()):
-				w = grafo.peso(u,v)
-		                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-		                        dist[v] = dist[u] + w
-	        for u in xrange(self.grafo.vertices()):
-		        for v in xrange(self.grafo.vertices()):
-		        	w = self.grafo.peso(u,v)
-	               		if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-					print "El grafo contiene un ciclo de peso negativo"
-					return
-	        self.distancias.append(dist) 
+        for src in xrange(self.grafo.vertices()):     
+            dist = [float("Inf")] * self.grafo.vertices()
+            dist[src] = 0
+            for _ in xrange(self.grafo.vertices() - 1):
+                for u in xrange(self.grafo.vertices()):
+                    for v in xrange(self.grafo.vertices()):
+                        w = grafo.peso(u,v)
+                        if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                                dist[v] = dist[u] + w
+            for u in xrange(self.grafo.vertices()):
+                for v in xrange(self.grafo.vertices()):
+                    w = self.grafo.peso(u,v)
+                    if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        print "El grafo contiene un ciclo de peso negativo"
+                        return
+            self.distancias.append(dist) 
 
-	 def distancia(self, u, v):
-	 	return self.distancias[u][v]
+    def distancia(self, u, v):
+        return self.distancias[u][v]
